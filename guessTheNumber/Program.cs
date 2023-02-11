05f5350d02a8f2c315aa8d53024dae7fa51edeaa
Dev-ops-Marko <107570524+Dev-ops-Marko@users.noreply.github.com>
2023-02-10 21:07:15 -0600

    Add files via upload

---
+++ guessTheNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace guessTheNumber
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int numberToGuess = random.Next(1, 100);
+            int guess;
+            do
+            {
+                Console.WriteLine("Adivina el numero entre 1 y 100, Ingresa un numero:");
+                guess = int.Parse(Console.ReadLine());
+                if (guess < numberToGuess)
+                {
+                    Console.WriteLine("Tu numero debe ser mayor");
+                }
+                else if ( guess > numberToGuess)
+                {
+                    Console.WriteLine("Tu numero debe ser menor");
+                }
+            } while (guess != numberToGuess);
+            Console.WriteLine("Acertaste!!1!!");
+        }
+    }
+}
